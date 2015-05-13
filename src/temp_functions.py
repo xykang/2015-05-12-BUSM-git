5f2080ccbdbc85ecb4861843abda0dada50f1620
Xinyu <temp@bu.edu>
2015-05-13 11:03:34 -0400

    Correct the floating problem

--- src/temp_functions.py
+++ src/temp_functions.py
@@ -2,7 +2,7 @@ def k_to_c(temp):
 	return temp - 273.15
 
 def f_to_k(temp):
-	return ((temp - 32) * (5 / 9)) + 273.15
+	return ((temp - 32) * (5 / 9.0)) + 273.15
 
 def f_to_c(temp):
 	temp_k = f_to_k(temp)
